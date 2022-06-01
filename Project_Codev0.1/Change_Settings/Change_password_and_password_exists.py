3b1c2c79851f33c75bdb51fc5306e710973c8903
JimTsintzelis <101722795+JimTsintzelis@users.noreply.github.com>
2022-06-01 17:25:17 +0300

    Create Change_password_and_password_exists.py

---
+++ Project_Codev0.1/Change_Settings/Change_password_and_password_exists.py
@@ -0,0 +1,13 @@
+a = int(input("Enter Password to verify your Identity: "))
+if a==123456
+    b = int(input("Enter new Password: "))
+    c = int(input("Confirm new Password: "))
+    if b==c
+        print("Your Password has changes succesfully")
+    else:
+        print("Your Password is not Confirmed")
+else:
+    print("unathorized acces")
+
+if b==a
+b = int(input("Please use a different Password: "))
