f95dcfa4b1ba3c033f5560769079d51a057655ee
JimTsintzelis <101722795+JimTsintzelis@users.noreply.github.com>
2022-06-11 18:13:46 +0300

    Create Moderator.py

---
+++ Project_Codev0.1/Class-diagram_Classes/Moderator.py
@@ -0,0 +1,36 @@
+class Moderator:
+
+
+"""
+ def __init__(self, age = 0):
+         self._age = age
+      
+    # getter method
+    def get_age(self):
+        return self._age
+      
+    # setter method
+    def set_age(self, x):
+        self._age = x
+"""
+    
+    def setModeratorName(self, ModeratorName:str):
+        self.ModeratorName = ModeratorName
+
+
+   
+    def getEventname(self):
+    return self.Eventname
+
+
+
+    def __init__(self, wallpapersEv):
+        self.wallpapersEV = wallpapersEv
+
+    def addWallpaperEvents(self, otherWallEv):
+        return Weight(self.wallpaperEv + otherWallEv.wallpapersEv)
+
+
+    def getSupportTicketName(self, SupportTicketName:str):
+        return self.SupportTicketName
+
