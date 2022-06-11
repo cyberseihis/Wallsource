49a3704ecda4caffacc3b6e3615cc31bce64b8f5
JimTsintzelis <101722795+JimTsintzelis@users.noreply.github.com>
2022-06-11 17:59:49 +0300

    Create E-support ticket.py

---
+++ Project_Codev0.1/E-support ticket.py
@@ -0,0 +1,10 @@
+Class ESupportTicket:
+
+
+ def getXrhsthsname(self):
+    return self.Xrhsthsname;
+
+
+
+ def setSupportTicketName(self, SupportTicketName:str):
+        self.SupportTicketName = SupportTicketName;
